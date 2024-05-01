c57263936e5ef53c71e256521383f90886783d18
Vikram Tripathi <121534517+vikramtripathi@users.noreply.github.com>
2024-05-01 13:35:10 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,17 @@
+package OPPs;
+
+public class Person {
+    
+        private String name; 
+      
+        // Getter
+        public String getName() {
+          return name;
+        }
+      
+        // Setter
+        public void setName(String newName) {
+          this.name = newName;
+        }
+      }
+
